b3baa7c0bd2338218301ce827fa25c0e7dada119
Bart King <github@cleardemon.com>
2016-04-27 15:26:47 +0100

    initial project creation and first TypeHelper implementation

---
+++ ClearDemon.Utility/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle ("ClearDemon.Utility")]
+[assembly: AssemblyDescription ("Utility functions and library support in a cross-platform world.")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("")]
+[assembly: AssemblyCopyright ("Bart King")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+[assembly: AssemblyVersion ("1.0.*")]
+
